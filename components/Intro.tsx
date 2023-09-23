7ae960321e387c72166a04464b89487cf8aacaf7
Swarup Lamichhane <lswarup1234@gmail.com>
2023-09-23 08:21:13 +0545

    Fixed row reverse

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -3,21 +3,22 @@ import { CustomButton } from ".";
 
 const Intro = () => {
   return (
-    <main className="flex mt-[140px] mb-[71px] justify-center items-center gap-[115px] flex-row-reverse sm:flex-row flex-wrap">
+    <main className="flex flex-wrap mt-[140px] mb-[71px] justify-center items-center gap-[115px] sm:flex-row">
       <div className="flex flex-col gap-8">
         <h1 className="intro-heading">
           Hi, I am Swarup,
           <br />
           MERN Stack Developer
         </h1>
-        <p className="w-[497px]">
+        <p className="max-w-[497px]">
           Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
           sint. Velit officia consequat duis enim velit mollit. Exercitation
           veniam consequat sunt nostrud amet.
         </p>
         <CustomButton title="Download Resume" />
       </div>
-      <div>
+      <div className="order-first lg:order-last">
+        {/* This will change the order of the div in small screens */}
         <Image
           src="/profile.jpg"
           alt="profile"
